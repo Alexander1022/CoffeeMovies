f0acf959c52deeedec5a2ee3df0a997180c8d8fb
Xander <36764735+Alexander1022@users.noreply.github.com>
2021-03-09 22:18:03 +0200

    late night update
    late night update

--- sw.js
+++ sw.js
@@ -2,64 +2,28 @@ var APP_PREFIX = 'CoffeeMovies'
 var VERSION = '_1.0.0'              
 var CACHE_NAME = APP_PREFIX + VERSION
 var URLS = [                            
-  '/CoffeeMovies/',                     
-  '/CoffeeMovies/index.html',
-  '/CoffeeMovies/css/style.css',
-  '/CoffeeMovies/images'           
+  '/',                     
+  '/index.html',
+  '/css/style.css',
+  '/images'           
 ]
 
-// Respond with cached resources
-self.addEventListener('fetch', function (e) 
+self.addEventListener('install', function(e) 
 {
-  console.log('fetch request : ' + e.request.url)
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) 
+    {
+      return cache.addAll(URLS);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) 
+{
   e.respondWith(
-    caches.match(e.request).then(function (request) 
+    caches.match(e.request).then(function(response) 
     {
-      if (request) 
-      { 
-        console.log('responding with cache : ' + e.request.url)
-        return request
-      } 
-      else 
-      {     
-        console.log('file is not cached, fetching : ' + e.request.url)
-        return fetch(e.request)
-      }
+      return response || fetch(e.request);
     })
-  )
-})
-
-self.addEventListener('install', function (e) 
-{
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) 
-    {
-      console.log('installing cache : ' + CACHE_NAME)
-      return cache.addAll(URLS)
-    })
-  )
-})
-
-self.addEventListener('activate', function (e) 
-{
-  e.waitUntil(
-    caches.keys().then(function (keyList) 
-    {
-      var cacheWhitelist = keyList.filter(function (key) 
-      {
-        return key.indexOf(APP_PREFIX)
-      })
-
-      cacheWhitelist.push(CACHE_NAME)
-
-      return Promise.all(keyList.map(function (key, i) 
-      {
-        if (cacheWhitelist.indexOf(key) === -1) 
-        {
-          console.log('deleting cache : ' + keyList[i] )
-          return caches.delete(keyList[i])
-        }
-      }))
-    })
-  )
-})
+  );
+});
